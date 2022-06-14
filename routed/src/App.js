4c44952118fc5e1060bc18a5c8f41587068ee5e7
Mo2024 <mohdosama2025@gmail.com>
2022-06-14 14:33:17 +0300

    Routes with params

--- routed/src/App.js
+++ routed/src/App.js
@@ -4,20 +4,40 @@ import Dog from './Dog'
 import Contact from './Contact'
 import About from './About'
 import { Routes, Route, NavLink } from 'react-router-dom';
+import Food from './patterns/Food'
 
+/* Section 20 Intro to routers */
+// function App() {
+//   return (
+//     <div className="App">
+//       <nav className='App-nav'>
+//         <NavLink className={({ isActive }) => (isActive ? "active-link" : '')} to='/dog'>Dog</NavLink>
+//         <NavLink className={({ isActive }) => (isActive ? "active-link" : '')} to='/contact'>Contact</NavLink>
+//         <NavLink className={({ isActive }) => (isActive ? "active-link" : '')} to='/'>About</NavLink>
+//       </nav>
+
+//       <Routes>
+//         <Route exact path="/dog" element={<Dog name="Muffins" />} />
+//         <Route exact path="/contact" element={<Contact />} />
+//         <Route exact path="/" element={<About />} />
+//       </Routes>
+
+//     </div>
+//   );
+// }
+
+/* Section 22: Patters */
 function App() {
   return (
     <div className="App">
-      <nav className='App-nav'>
+      {/* <nav className='App-nav'>
         <NavLink className={({ isActive }) => (isActive ? "active-link" : '')} to='/dog'>Dog</NavLink>
         <NavLink className={({ isActive }) => (isActive ? "active-link" : '')} to='/contact'>Contact</NavLink>
         <NavLink className={({ isActive }) => (isActive ? "active-link" : '')} to='/'>About</NavLink>
-      </nav>
+      </nav> */}
 
       <Routes>
-        <Route exact path="/dog" element={<Dog name="Muffins" />} />
-        <Route exact path="/contact" element={<Contact />} />
-        <Route exact path="/" element={<About />} />
+        <Route exact path="/food/:name" element={<Food />} />
       </Routes>
 
     </div>
---
+++ routed/src/patterns/Food.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import { Navigate, useParams } from "react-router-dom";
+import "./Food.css";
+
+function Food() {
+  const { name } = useParams();
+  const url = `https://source.unsplash.com/1600x900/?${name}`;
+  return (
+    <div className='Food'>
+      {/\d/.test(name) ? (<Navigate to='/' />) :
+        (
+          <div>
+            <h1>I love to eat {name}</h1>
+            <img src={url} alt={name} />
+          </div>
+        )}
+    </div>
+  );
+}
+
+export default Food;
